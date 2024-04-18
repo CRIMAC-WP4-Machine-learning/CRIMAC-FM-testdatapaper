e784e35230f3c38669db10cabde1296bf30b2163
Handegard <nilsolav@styx.hi.no>
2024-04-18 12:20:45 +0200

    Get data modified to move data from crimac to crimac scratch

--- get_data.py
+++ get_data.py
@@ -1,28 +1,27 @@
-import pandas as pd
 import os
-import subprocess
+import csv
 
-# NB: Copy the DataSets.csv from https://github.com/CRIMAC-WP4-Machine-learning/CRIMAC-data-organisation/blob/main/DataSets.csv
+# this script copy the data from the backed up crimac stire to the S3 via crimac scratch
 
 # Read metadata
-df = pd.read_csv('DataSets.csv')
-testdata = df[(df['pulsetype'] == 'FM') &
-              (df['dataset'].str[0] == 'T')]
-
-testdata.to_csv('testdata.csv')
+testdata = []
+with open("testdata.csv", 'r') as file:
+    csvreader = csv.reader(file)
+    header = next(csvreader)
+    for row in csvreader:
+        testdata.append(row[1])
 
 # Call whoami and capture the output
-user = subprocess.run(['whoami'], capture_output=True, text=True).stdout.strip()
 source = '/data/crimac/'
-dest = '/mnt/c/DATAscratch/to_NR'
+dest = '/data/crimac-scratch/CRIMAC-FM-testdata/'
 
-for _testdata in testdata['dataset']:
+for _testdata in testdata:
     print(_testdata)
     if not os.path.exists(dest + '/' + _testdata[1:5]+'/'):
         mkdir = 'mkdir '+dest + '/' + _testdata[1:5]+'/'
         print(mkdir)
         os.system(mkdir)
-    scp = 'scp -r '+user+'@dedun.hi.no:'+source+_testdata[1:5]+'/'+_testdata + ' ' + dest + '/' + _testdata[1:5]+'/'
+    scp = 'scp -r '+source+_testdata[1:5]+'/'+_testdata + ' ' + dest + _testdata[1:5]+'/'
     print(scp)
     os.system(scp)
 
