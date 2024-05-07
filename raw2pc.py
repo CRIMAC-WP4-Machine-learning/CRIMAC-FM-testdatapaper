ea474d1f07045220a48d47214abc2c392308a070
Nils Olav Handegard <nils.olav.handegard@imr.no>
2024-05-07 15:06:13 +0200

    Wrapper code for the splitting. Need info from the raw files.

--- raw2pc.py
+++ raw2pc.py
@@ -17,24 +17,54 @@ the results as an netcdf. the NetCDF file is read and the pulse compressed data 
 """
 
 
-def raw2pc(inputdir, outputdir):
-    # Instanitate the class
-    ksi = ks.KoronaScript()
+def raw2pc(inputdir, outputdir, split):
+    # Ketil: The parameters below are needed from the raw files. Can we use ektools? It is testdataset
+    # T2023002 that needs splitting. The other dont. Ideally we need code to do this from the data
+    # themselves with out the logic of split=='yes'
+    
+    if split == 'yes':
+        # Define channels to keep in each splitting operation
+        channels = {"CW" : [1, 5, 9, 13, 17],
+                    "FM2ms" : [2, 6, 10, 14, 18],
+                    "FM4ms" : [3, 7, 11, 15, 19],
+                    "FM2msSLOW" : [4, 8, 12, 16, 20]}
 
-    # Add emptypingremoval module
-    ksi.add(ksm.EmptyPingRemoval())
-    # Add the pulsecompression module and write to nc
-    ksi.add(ksm.NetcdfWriter(Active = "true",
-                             DirName = "pc",
-                             MainFrequency = "200",
-                             WriterType = "CHANNEL_GROUPS",
-                             GriddedOutputType = "PULSE_COMPRESSION",
-                             WriteAngels = "true",
-                             FftWindowSize = "2",
-                             DeltaFrequency = "1",
-                             ChannelGroupOutputType = "PULSE_COMPRESSION"))
-    ksi.write()
-    ksi.run(src=inputdir, dst=outputdir) # Begrening på kjernar
+        # Define comment to include in each split dataset
+        comments = {"CW" : "CW_0256ms",
+                    "FM2ms" : "FM_2ms_FAST_taper",
+                    "FM4ms" : "FM_4ms_FAST_taper",
+                    "FM2msSLOW" : "FM_2ms_SLOW_taper"}
+    else:
+        channels = {"FM2ms" : [1, 2, 3, 4]} # this is a hack, must know the number of channels in the data set
+        # Define comment to include in each split dataset
+        comments = {"FM2ms" : "FM_2ms_FAST_taper"}
+    # /Ketil: End of help needed
+
+    for name, channel, comment in zip(channels, channels.values(), comments.values()):
+        
+        # Instantiate the class
+        ksi = ks.KoronaScript()
+        # Add emptypingremoval module
+        ksi.add(ksm.EmptyPingRemoval())
+        # Add comment
+        ksi.add(ksm.Comment(LineBreak='false', Label=comment))
+        # Remove channels
+        ksi.add(ksm.ChannelRemoval(Channels=channel, KeepSpecified='true'))
+        # Add the pulsecompression module and write to nc
+        ksi.add(ksm.NetcdfWriter(Active = "true",
+                                 DirName = name, # "pc", # Use channel?
+                                 MainFrequency = "200", # this must be added to the metadata
+                                 WriterType = "CHANNEL_GROUPS",
+                                 GriddedOutputType = "PULSE_COMPRESSION",
+                                 WriteAngels = "true",
+                                 FftWindowSize = "2",
+                                 DeltaFrequency = "1",
+                                 ChannelGroupOutputType = "PULSE_COMPRESSION"))
+        # Print the configuration
+        ksi.write()
+        # Run KoronaScript
+        ksi.run(src=inputdir, dst=outputdir) # Begrening på kjernar
+
     # Remove temporary korona files
     kfiles = [os.remove(_f) for _f in glob.glob(outputdir+'/*korona.*')]
 
@@ -66,11 +96,11 @@ def pc2png(outputdir):
 
 
 # Read metadata & env variables
-df = pd.read_csv('testdata.csv')
+df = pd.read_csv('testdata.csv').iloc[[10, 12], 0:6] # Subset data set for testing
 crimac = os.getenv('CRIMACSCRATCH')
 
 # Print the current test data sets
-for _dataset in df['dataset']:
+for _dataset, split in zip(df['dataset'], df['split']):
     print(_dataset)
     inputdir = os.path.join(crimac, 'CRIMAC-FM-testdata', _dataset[1:5],
                             _dataset, 'ACOUSTIC',
@@ -82,5 +112,5 @@ for _dataset in df['dataset']:
     if os.path.exists(inputdir):
         print(inputdir)
         print(outputdir)
-        raw2pc(inputdir, outputdir)
-        pc2png(outputdir)
+        raw2pc(inputdir, outputdir, split)
+        # pc2png(outputdir)
