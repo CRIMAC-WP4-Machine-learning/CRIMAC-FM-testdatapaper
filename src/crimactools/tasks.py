dff0526b0d239dba70173a1f17d0504c20304635
Ketil Malde <ketil@malde.org>
2026-05-26 14:57:50 +0200

    One more fix, and more logging output

--- src/crimactools/tasks.py
+++ src/crimactools/tasks.py
@@ -62,12 +62,12 @@ def list_datasets(dataset_id: str | None = None) -> list:
 
 def get_dataset(datadir: Path, dataset_id: str, url: str, dry_run: bool = False):
 
-    logger.info(f"Downloading {url} to {datadir}")
+    logger.info(f'Downloading {url} to "{datadir}"')
 
     if not Path(datadir).exists():
         logger.info(f'Creating data directory "{datadir}"')
         Path(datadir).mkdir(parents=True, exist_ok=True)
-    elif not Path.is_dir(datadir):
+    elif not Path(datadir).is_dir():
         logger.error(f'Data dirctory "{datadir}" exists, but is not a directory')
 
     # Get standard folder structure
